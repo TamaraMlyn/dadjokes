47c2f5fbeee9e1bf8b524be61ef5b119b04d7b38
Tamara Mlýnková <tamara.mlynkova@gmail.com>
2021-05-29 10:11:38 +0200

    Hotový úkol 14

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import './style.css';
 
-const App = () => (
-  <div class="container">
+const App = () => {
+  const [likesUp, setLikesUp] = useState(0);
+  const [likesDown, setLikesDown] = useState(0);
+
+  const likeElm = document.querySelector('#likes-up');
+  const dislikeElm = document.querySelector('#likes-down');
+
+  const handleClickLikesUp = () => {
+    setLikesUp(likesUp + 1);
+  };
+
+  const handleClickLikesDown = () => {
+    setLikesDown(likesDown + 1);
+  };
+
+  return (
+    <div class="container">
       <div class="joke">
         <div class="joke__body">
           <div class="joke__user">
-            <img class="user-avatar" src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png" />
+            <img
+              class="user-avatar"
+              src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png"
+            />
             <p class="user-name">Neroxx</p>
           </div>
 
@@ -18,13 +36,26 @@ const App = () => (
           </p>
         </div>
         <div class="joke__likes">
-          <button id="btn-up" class="btn-like btn-like--up"></button>
-          <span id="likes-up" class="likes-count likes-count--up">0</span>
-          <button id="btn-down" class="btn-like btn-like--down"></button>
-          <span id="likes-down" class="likes-count likes-count--down">0</span>
+          <button
+            id="btn-up"
+            class="btn-like btn-like--up"
+            onClick={handleClickLikesUp}
+          ></button>
+          <span id="likes-up" class="likes-count likes-count--up">
+            {likesUp}
+          </span>
+          <button
+            id="btn-down"
+            class="btn-like btn-like--down"
+            onClick={handleClickLikesDown}
+          ></button>
+          <span id="likes-down" class="likes-count likes-count--down">
+            {likesDown}
+          </span>
         </div>
       </div>
     </div>
-);
+  );
+};
 
 render(<App />, document.querySelector('#app'));
